4418113e612b4a596e0321f28b02b45ea9164af6
bspena65 <bspena65@ucatolica.edu.co>
2023-10-24 11:35:55 -0500

    actualizacion roles

--- src/main/java/com/ucatolica/ecommerce/enums/Role.java
+++ src/main/java/com/ucatolica/ecommerce/enums/Role.java
@@ -2,10 +2,9 @@ package com.ucatolica.ecommerce.enums;
 
 /**
  * Enumeración que define los roles de usuario disponibles en la aplicación.
- * Los roles posibles son: user, manager y admin.
+ * Los roles posibles son: user y admin.
  */
 public enum Role {
     user,
-    manager,
     admin
 }
