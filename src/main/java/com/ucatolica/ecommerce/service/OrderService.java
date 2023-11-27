fd79f4d01436e6cc717fb3e67ef78e853c0134c8
bspena65 <bspena65@ucatolica.edu.co>
2023-11-27 13:08:03 -0500

    actualizacion de orden

--- src/main/java/com/ucatolica/ecommerce/service/OrderService.java
+++ src/main/java/com/ucatolica/ecommerce/service/OrderService.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
